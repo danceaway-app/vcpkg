40b31e831612e9fb87f6b842f0ccefd7e64bc489
gemelen <gemelen@danceaway.app>
2025-12-11 23:22:57 -0300

    [react-native-callinvoker] 0.83.0 port files

--- ports/react-native-callinvoker/portfile.cmake
+++ ports/react-native-callinvoker/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO facebook/react-native
-    REF v0.82.0
-    SHA512 40bd1cf83686deed01dbe2550086f538625c75ad879c0ad3c3724c63ab97b2be048f877e7456de694e3f95833facf07db79bd0e9c4239b37acb7acc5ae80f2bc
+    REF v0.83.0
+    SHA512 e7caf9aa0f3a0b70d00c03b7c1b53b9b105a5c15975bd289e829ee8aba230fb061878fe79d613dddf3792760cb23b5f0fb5724020d12568d5b94dc590c25b987
     HEAD_REF main
 )
 
